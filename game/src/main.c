e6740281e16451341db2470c5ff53df6bf08e61c
NoahBilmer <noahbilmer@gmail.com>
2023-12-26 22:45:10 -0500

    Added functions for representing the board and also added logic to ensure the window scales properly.

--- game/src/main.c
+++ game/src/main.c
@@ -1,107 +1,117 @@
 /*******************************************************************************************
-*
-*   raylib [core] example - window scale letterbox (and virtual mouse)
-*
-*   Example originally created with raylib 2.5, last time updated with raylib 4.0
-*
-*   Example contributed by Anata (@anatagawa) and reviewed by Ramon Santamaria (@raysan5)
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2019-2023 Anata (@anatagawa) and Ramon Santamaria (@raysan5)
-*
+* Tetris in C by Noah Bilmer
 ********************************************************************************************/
-
+#include <stdlib.h>
 #include "raylib.h"
-
+#include "headers/board.h"
+#include "headers/globals.h"
 #include "raymath.h"        // Required for: Vector2Clamp()
+#include <stdio.h>
 
 #define MAX(a, b) ((a)>(b)? (a) : (b))
 #define MIN(a, b) ((a)<(b)? (a) : (b))
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+Texture2D gameboardUI;
+Texture2D blocksSpriteSheet;
+Texture2D digitsSpriteSheet;
+
+void drawBlock(int x, int y, int color, float scale);
+void drawBoard(int (*board)[ROWS],float scale);
+
 int main(void)
 {
+
     const int windowWidth = 800;
-    const int windowHeight = 450;
+    const int windowHeight = 800;
+    int (*fallingBoard)[ROWS] = calloc(4,sizeof(int[ROWS][COLUMNS]));
+    int (*landedBoard)[ROWS] = calloc(4,sizeof(int[ROWS][COLUMNS]));
 
     // Enable config flags for resizable window and vertical synchro
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
-    InitWindow(windowWidth, windowHeight, "raylib [core] example - window scale letterbox");
+    InitWindow(windowWidth, windowHeight, "Tetris");
+    // Move from bin/Debug to tetris
+    gameboardUI = LoadTexture("resources/tetris-ui.png");
+    blocksSpriteSheet = LoadTexture("resources/blocks.png");
+    digitsSpriteSheet = LoadTexture("resources/digits.png");
     SetWindowMinSize(320, 240);
-
-    int gameScreenWidth = 640;
-    int gameScreenHeight = 480;
-
+    
     // Render texture initialization, used to hold the rendering result so we can easily resize it
-    RenderTexture2D target = LoadRenderTexture(gameScreenWidth, gameScreenHeight);
+    RenderTexture2D target = LoadRenderTexture(SCREEN_W, SCREEN_H);
     SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);  // Texture scale filter to use
 
-    Color colors[10] = { 0 };
-    for (int i = 0; i < 10; i++) colors[i] = (Color){ GetRandomValue(100, 250), GetRandomValue(50, 150), GetRandomValue(10, 100), 255 };
+    SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
+    #if DEBUG == 1
+    for (int row = 0; row < ROWS; row++) {
+        printf("\n");
+        for (int col = 0; col < COLUMNS; col++) {
+            printf("%d, ", fallingBoard[row][col]);
+        }
+    }  
+    #endif
+   
+    writeBlocks(-1,-1,o_block[0],fallingBoard);
+    
+   
+    printf("\n");
     // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
         // Compute required framebuffer scaling
-        float scale = MIN((float)GetScreenWidth()/gameScreenWidth, (float)GetScreenHeight()/gameScreenHeight);
-
+        float scale = MIN((float)GetScreenWidth()/SCREEN_W, (float)GetScreenHeight()/SCREEN_H);
+        
         if (IsKeyPressed(KEY_SPACE))
         {
-            // Recalculate random colors for the bars
-            for (int i = 0; i < 10; i++) colors[i] = (Color){ GetRandomValue(100, 250), GetRandomValue(50, 150), GetRandomValue(10, 100), 255 };
+        
         }
-
-        // Update virtual mouse (clamped mouse value behind game screen)
-        Vector2 mouse = GetMousePosition();
-        Vector2 virtualMouse = { 0 };
-        virtualMouse.x = (mouse.x - (GetScreenWidth() - (gameScreenWidth*scale))*0.5f)/scale;
-        virtualMouse.y = (mouse.y - (GetScreenHeight() - (gameScreenHeight*scale))*0.5f)/scale;
-        virtualMouse = Vector2Clamp(virtualMouse, (Vector2){ 0, 0 }, (Vector2){ (float)gameScreenWidth, (float)gameScreenHeight });
-
-        // Apply the same transformation as the virtual mouse to the real mouse (i.e. to work with raygui)
-        //SetMouseOffset(-(GetScreenWidth() - (gameScreenWidth*scale))*0.5f, -(GetScreenHeight() - (gameScreenHeight*scale))*0.5f);
-        //SetMouseScale(1/scale, 1/scale);
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        // Draw everything in the render texture, note this will not be rendered on screen, yet
-        BeginTextureMode(target);
-            ClearBackground(RAYWHITE);  // Clear render texture background color
-
-            for (int i = 0; i < 10; i++) DrawRectangle(0, (gameScreenHeight/10)*i, gameScreenWidth, gameScreenHeight/10, colors[i]);
-
-            DrawText("If executed inside a window,\nyou can resize the window,\nand see the screen scaling!", 10, 25, 20, WHITE);
-            DrawText(TextFormat("Default Mouse: [%i , %i]", (int)mouse.x, (int)mouse.y), 350, 25, 20, GREEN);
-            DrawText(TextFormat("Virtual Mouse: [%i , %i]", (int)virtualMouse.x, (int)virtualMouse.y), 350, 55, 20, YELLOW);
-        EndTextureMode();
         
         BeginDrawing();
             ClearBackground(BLACK);     // Clear screen background
-
-            // Draw render texture to screen, properly scaled
-            DrawTexturePro(target.texture, (Rectangle){ 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
-                           (Rectangle){ (GetScreenWidth() - ((float)gameScreenWidth*scale))*0.5f, (GetScreenHeight() - ((float)gameScreenHeight*scale))*0.5f,
-                           (float)gameScreenWidth*scale, (float)gameScreenHeight*scale }, (Vector2){ 0, 0 }, 0.0f, WHITE);
+            // Draw the UI texture to screen, properly scaled
+            DrawTexturePro(gameboardUI, (Rectangle){ 0.0f, 0.0f, (float)target.texture.width, (float)target.texture.height }, 
+                           (Rectangle){ (GetScreenWidth() - ((float)SCREEN_W*scale))*0.5f, (GetScreenHeight() - ((float)SCREEN_H*scale))*0.5f,
+                           (float)SCREEN_W*scale, (float)SCREEN_H*scale }, 
+                           (Vector2){ 0.0f, 0.0f }, 
+                           0.0f, 
+                           WHITE);
+        drawBoard(fallingBoard,scale);
         EndDrawing();
-        //--------------------------------------------------------------------------------------
     }
 
     // De-Initialization
-    //--------------------------------------------------------------------------------------
     UnloadRenderTexture(target);        // Unload render texture
-
     CloseWindow();                      // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    free(fallingBoard);
     return 0;
 }
+
+
+/**
+* FUNCTION: drawBoard() 
+* DESCRIPTION: Draws the provided board to the screen.
+*/ 
+void drawBoard(int (*board)[ROWS],float scale) {
+    for (int row = 0; row < ROWS; row++) {
+        for (int col = 0; col < COLUMNS; col++) {
+            if (board[row][col] != 0)
+                drawBlock(col,row,board[row][col],scale);
+        }
+    }
+}
+
+/**
+* FUNCTION: drawBlock() 
+* DESCRIPTION: Draws the provided block to the screen.
+*/ 
+void drawBlock(int x, int y, int color,float scale) {
+    float blockSize = 0.05;
+    DrawTexturePro(blocksSpriteSheet, (Rectangle){ 0.0f, color*32.0f, 32,32 }, 
+                           (Rectangle){ (GetScreenWidth() - ((float)SCREEN_W*scale))*0.5f, (GetScreenHeight() - ((float)SCREEN_H*scale))*0.5f,
+                           (float)SCREEN_W*scale*blockSize, (float)SCREEN_H*scale*0.043 }, 
+                           (Vector2){ -1*(60.0f + 32*x)*scale, -1*(14.0f + 32*(y))*scale }, 
+                           0.0f, 
+                           WHITE);
+}
+
+
+
