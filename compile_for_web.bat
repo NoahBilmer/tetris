27491875492932432d4f0bcad2e5207e040f9e85
NoahBilmer <54690983+NoahBilmer@users.noreply.github.com>
2025-01-20 20:53:08 -0500

    fixed typo

--- compile_for_web.bat
+++ compile_for_web.bat
@@ -1,4 +1,4 @@
-REM Note that the PLATFORM_WEB #define in globals.h should be uncommented in
+REM Note that the PLATFORM_WEB #define in globals.h should be uncommented in order for the
 REM game to work properly.
 echo Compiling for web...
 emcc -o tetris.html -s EXPORTED_FUNCTIONS=["_free","_malloc","_main"] -s EXPORTED_RUNTIME_METHODS=ccall ^
